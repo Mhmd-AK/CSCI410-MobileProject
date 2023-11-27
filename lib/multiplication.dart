69d02e2e2e8c95180b31c7e5d0d6ff7e3f55934f
Mhmd-AK <ma0202232@gmail.com>
2023-11-27 23:15:39 +0200

    new files

---
+++ lib/multiplication.dart
@@ -0,0 +1,145 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'const.dart';
+import 'my_button.dart';
+import 'result_message.dart';
+
+class Mult extends StatefulWidget {
+  const Mult({Key? key}) : super(key: key);
+
+  @override
+  State<Mult> createState() => _Mult();
+}
+
+class _Mult extends State<Mult> {
+  List<String> numberPad = [
+    '7',
+    '8',
+    '9',
+    'C',
+    '4',
+    '5',
+    '6',
+    '=',
+    '3',
+    '2',
+    '1',
+    '0',
+  ];
+  int numberA = 1;
+  int numberB = 1;
+  String userAnswer = '';
+  void buttonTapped(String button) {
+    setState(() {
+      if (button == '=') {
+        checkResult();
+      } else if (button == 'C') {
+        userAnswer = '';
+      } else if (userAnswer.length < 3) {
+        userAnswer += button;
+      }
+    });
+  }
+  void checkResult() {
+    if (numberA * numberB == int.parse(userAnswer)) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return ResultMessage(
+              message: 'Correct!',
+              onTap: goToNextQuestion,
+              icon: Icons.arrow_forward,
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return ResultMessage(
+              message: 'Sorry try again',
+              onTap: goBackToQuestion,
+              icon: Icons.rotate_left,
+            );
+          });
+    }
+  }
+  var randomNumber = Random();
+
+  void goToNextQuestion() {
+    Navigator.of(context).pop();
+    setState(() {
+      userAnswer = '';
+    });
+    numberA = randomNumber.nextInt(10);
+    numberB = randomNumber.nextInt(10);
+  }
+
+  void goBackToQuestion() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Multiplication',style: TextStyle(fontSize: 32),),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+        toolbarHeight: 150,
+      ),
+      backgroundColor: Colors.grey,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      numberA.toString() + ' x ' + numberB.toString() + ' = ',
+                      style: whiteTextStyle,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Center(
+                        child: Text(
+                          userAnswer,
+                          style: whiteTextStyle,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: GridView.builder(
+                itemCount: numberPad.length,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return MyButton(
+                    child: numberPad[index],
+                    onTap: () => buttonTapped(numberPad[index]),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
