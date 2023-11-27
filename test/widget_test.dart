ff0fc7449f18939fbd3a7667cbce5f2f61bc320c
Mhmd-AK <ma0202232@gmail.com>
2023-11-27 22:54:11 +0200

    test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobileproject1/main.dart';
+import 'package:mobileproject1/test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
