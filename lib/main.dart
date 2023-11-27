3b9da4545a7106f7e8d028cfa734a24c65f3f2e0
souadkhalifeh <souadkhalifeh620@gmail.com>
2023-11-27 23:33:53 +0200

    main edited

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Math Game',
       debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
     );
